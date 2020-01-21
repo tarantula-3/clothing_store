4e03c555305da8c3e4b97a4763b50b653ccc94f0
ming <tarantula.project@gmail.com>
2020-01-21 13:19:53 +0700

    created superuser, add img_path to Item

--- clothing/models.py
+++ clothing/models.py
@@ -12,6 +12,7 @@ class Item(models.Model):
     name = models.CharField(max_length=128)
     price = models.FloatField()
     type = models.ForeignKey(ClothesType, on_delete=models.PROTECT)
+    img_path = models.CharField(max_length=5000)
 
     def __str__(self):
         return self.name
