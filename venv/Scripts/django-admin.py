adf88c4f743ea27b6381ce8a9403a5cb2cb549de
ming <tarantula.project@gmail.com>
2020-01-21 12:52:08 +0700

    installed django

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin\Documents\Python\clothing_store\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
