078d5c047d927319b7a4e3dd7e5826fc1bb7d98c
ming <tarantula.project@gmail.com>
2020-01-24 23:42:44 +0700

    customized admin site with list_display

--- clothing/admin.py
+++ clothing/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import ClothesType, Item
 
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'type',
+    )
+
+
 admin.site.register(ClothesType)
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
