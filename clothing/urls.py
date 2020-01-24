f94f286840649fb5c3f5d6035415eb9418c2e3d6
ming <tarantula.project@gmail.com>
2020-01-24 23:06:27 +0700

    created all_product's urls, views, html file

--- clothing/urls.py
+++ clothing/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'clothing'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all_products/', views.all_product, name='all_products'),
 ]
--- clothing/views.py
+++ clothing/views.py
@@ -4,3 +4,9 @@ from .models import Item, ClothesType
 
 def index(request):
     return render(request, 'clothing/index.html')
+
+
+def all_product(request):
+    items = Item.objects.all()
+    context = {'items': items}
+    return render(request, 'clothing/all_products.html', context=context)
